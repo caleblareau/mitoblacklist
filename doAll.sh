11b274e5fa66a7bc461cca29dc960c24ed376ed0
caleblareau <caleblareau@g.harvard.edu>
2017-04-23 14:56:22 -0400

    fastq generation script good

--- doAll.sh
+++
@@ -1,1 +0,0 @@
-./exec/art_illumina -ss NS50 -c 1000 -i fasta/hg38_chrM.fa -l 20 -o fastq/hg38_dat
---
+++ fastqMake.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NREADS=100
+
+genomes="hg19 hg38 mm9 mm10"
+
+for genome in $genomes
+do
+	./exec/art_illumina -ss NS50 -c $NREADS -i "fasta/${genome}_chrM.fa" -l 20 -o "fastq/${genome}_dat"
+	rm "fastq/${genome}_dat.aln"
+	gzip "fastq/${genome}_dat.fq"
+done
