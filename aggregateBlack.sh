cfeb1e1027b47223b2b10e66b9e6cb59065fea23
caleblareau <caleblareau@g.harvard.edu>
2017-06-12 17:51:49 -0400

    species mixed blacklist

--- aggregateBlack.sh
+++ aggregateBlack.sh
@@ -8,3 +8,11 @@ cat encodeBlacklist/hg19.encode.blacklist.bed peaks/hg19_peaks.narrowPeak | awk 
 cat encodeBlacklist/mm9.encode.blacklist.bed peaks/mm9_peaks.narrowPeak | awk '{print $1"\t"$2"\t"$3}' | sortBed | mergeBed -i stdin -d $DIST  > combinedBlacklist/mm9.full.blacklist.bed
 cat encodeBlacklist/hg38.encode.blacklist.bed peaks/hg38_peaks.narrowPeak | awk '{print $1"\t"$2"\t"$3}' | sortBed | mergeBed -i stdin -d $DIST  > combinedBlacklist/hg38.full.blacklist.bed
 cat encodeBlacklist/mm10.encode.blacklist.bed peaks/mm10_peaks.narrowPeak | awk '{print $1"\t"$2"\t"$3}' | sortBed | mergeBed -i stdin -d $DIST  > combinedBlacklist/mm10.full.blacklist.bed
+
+# make species mixed blacklist
+awk '{print substr($0,4)}' combinedBlacklist/hg19.full.blacklist.bed | awk '{print "human"$0}' > combinedBlacklist/hg19_mm10_c.full.blacklist.bed
+
+awk '{print substr($0,4)}' combinedBlacklist/mm10.full.blacklist.bed | awk '{print "mouse"$0}' >> combinedBlacklist/hg19_mm10_c.full.blacklist.bed
+
+
+
